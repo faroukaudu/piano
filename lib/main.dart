8b8b4b9620ca357f4506a99990c56a009a6a283c
farouk audu <fagzy99@gmail.com>
2021-03-01 08:18:50 +0100

    First changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MySong());
+
+class MySong extends StatefulWidget {
+  @override
+  _MySongState createState() => _MySongState();
+}
+
+class _MySongState extends State<MySong> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
