302ab4d0fefd39554cf2f964edabd804d56ad577
farouk audu <fagzy99@gmail.com>
2021-03-01 08:58:02 +0100

    First changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,44 @@ class MySong extends StatefulWidget {
   _MySongState createState() => _MySongState();
 }
 
+void keySound(int num) {
+  final player = AudioCache();
+  player.play('note$num.wav');
+}
+
+Expanded sound({Color col, int pin}) {
+  return Expanded(
+    child: FlatButton(
+      color: col,
+      onPressed: () {
+        keySound(pin);
+      },
+      child: null,
+    ),
+  );
+}
+
 class _MySongState extends State<MySong> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              sound(col: Colors.purple, pin: 1),
+              sound(col: Colors.brown, pin: 2),
+              sound(col: Colors.blue, pin: 3),
+              sound(col: Colors.red, pin: 4),
+              sound(col: Colors.black, pin: 5),
+              sound(col: Colors.yellow, pin: 6),
+              sound(col: Colors.green, pin: 7),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
